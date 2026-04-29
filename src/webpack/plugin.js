cdc0d5fd3ed91bbf6874e99390711d02332d7a98
zc <2064281269@qq.com>
2026-04-29 16:44:15 +0800

    perf: webpack配置优化

--- src/webpack/plugin.js
+++ src/webpack/plugin.js
@@ -1,17 +1,55 @@
+const fs = require('fs')
+const path = require('path')
 const { createRequire } = require('module')
 
+const PLUGIN_NAME = 'ConsoleLinkWebpackPlugin'
+const DEVTOOL_SOURCE_PATH = path.resolve(__dirname, '../utils/console-devtool-function.js')
+
 /**
- * Webpack 插件：在 HTML 中注入 console-devtool-function.js
- * 兼容 html-webpack-plugin v3/v4+
+ * Webpack plugin: inject console-devtool-function.js into HTML
+ * and emit the runtime file for html-webpack-plugin v3/v4/v5.
  */
 function ConsoleLinkWebpackPlugin(userOptions = {}) {
   const options = Object.assign(
     {
       injectDevtoolFunction: false,
-      scriptPath: './src/utils/console-devtool-function.js'
+      scriptPath: '',
+      filePath: ''
     },
     userOptions
   )
+
+  function validateOptions() {
+    if (!options.injectDevtoolFunction) return
+
+    if (!options.scriptPath || !options.filePath) {
+      throw new Error(
+        `[${PLUGIN_NAME}] scriptPath and filePath are required when injectDevtoolFunction is true`
+      )
+    }
+  }
+
+  function emitDevtoolAsset(compilation, compiler) {
+    if (!options.injectDevtoolFunction) return
+
+    const assetContent = fs.readFileSync(DEVTOOL_SOURCE_PATH, 'utf-8')
+    const normalizedFilePath = String(options.filePath).replace(/^\/+/, '')
+
+    if (typeof compilation.emitAsset === 'function' && compiler.webpack && compiler.webpack.sources) {
+      const { RawSource } = compiler.webpack.sources
+      compilation.emitAsset(normalizedFilePath, new RawSource(assetContent))
+      return
+    }
+
+    compilation.assets[normalizedFilePath] = {
+      source() {
+        return assetContent
+      },
+      size() {
+        return Buffer.byteLength(assetContent, 'utf8')
+      }
+    }
+  }
 
   function injectIntoHtml(data, cb) {
     if (!options.injectDevtoolFunction) return cb(null, data)
@@ -44,21 +82,31 @@ function ConsoleLinkWebpackPlugin(userOptions = {}) {
   }
 
   this.apply = function (compiler) {
-    compiler.hooks.compilation.tap('ConsoleLinkWebpackPlugin', function (compilation) {
+    validateOptions()
+
+    compiler.hooks.emit.tapAsync(PLUGIN_NAME, function (compilation, cb) {
+      try {
+        emitDevtoolAsset(compilation, compiler)
+        cb()
+      } catch (error) {
+        cb(error)
+      }
+    })
+
+    compiler.hooks.compilation.tap(PLUGIN_NAME, function (compilation) {
       const HtmlWebpackPlugin = resolveHtmlWebpackPlugin(compiler)
 
-      // html-webpack-plugin v4/v5
       if (HtmlWebpackPlugin && typeof HtmlWebpackPlugin.getCompilationHooks === 'function') {
         const hooks = HtmlWebpackPlugin.getCompilationHooks(compilation)
-        hooks.beforeEmit.tapAsync('ConsoleLinkWebpackPlugin', injectIntoHtml)
+        hooks.beforeEmit.tapAsync(PLUGIN_NAME, injectIntoHtml)
         return
       }
 
-      // html-webpack-plugin v3（Vue CLI 4 常见）
       if (compilation.hooks && compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing) {
-        compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('ConsoleLinkWebpackPlugin', injectIntoHtml)
+        compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(PLUGIN_NAME, injectIntoHtml)
         return
       }
+
       if (typeof compilation.plugin === 'function') {
         compilation.plugin('html-webpack-plugin-before-html-processing', injectIntoHtml)
       }
